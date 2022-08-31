0a71de16d3cc48ff9f78512bb62dab893edd8b66
Elias Consalvo <econs013@fiu.edu>
2022-08-30 22:17:16 -0400

    test

--- pages/context/userContext.js
+++ pages/context/userContext.js
@@ -3,4 +3,4 @@ import { createContext } from 'react';
  const UserContext = createContext();
  const LogContext = createContext();
 
- export {UserContext, LogContext};
+ export default {UserContext, LogContext};
